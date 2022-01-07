9bd2862b8bf6186f2adcb6a6250eb204e1341d71
Yeamin Mahmud Resin <59418750+mahmudresin@users.noreply.github.com>
2022-01-08 04:35:39 +0600

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yeami
+ */
+import java.util.Random;
+
+public class RandomNumber {
+    public int random =  (int)(Math.random()*(9999-999+1)+999);
+    
+}
